28f1918e49b27d3d51429fce651acd6ea6a341b7
satzsaravanan <ajoashok7@gmail.com>
2021-02-18 11:40:18 +0530

    first

---
+++ src/org/thor/WindSheild.java
@@ -0,0 +1,14 @@
+package org.thor;
+
+public class WindSheild {
+
+	private void doctorstrange() {
+	System.out.println("capt america");
+
+	}
+	public static void main(String[] args) {
+		 WindSheild w=new WindSheild();
+		 w.doctorstrange();
+		 
+	}
+}
